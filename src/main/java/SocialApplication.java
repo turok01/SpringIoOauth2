1b030855b8b7c1f55fc9b9caec0c3f0e98dac9bd
turok01 <toolbox@tut.by>
2020-01-23 23:46:58 +0300

    Add application YAML

---
+++ src/main/java/SocialApplication.java
@@ -0,0 +1,7 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+public class SocialApplication {
+}
